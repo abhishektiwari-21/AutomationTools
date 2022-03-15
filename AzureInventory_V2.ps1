c3e21f8f70d44f85a4c6c60b8983a50cc0f39d56
abhishektiwari-21 <101660439+abhishektiwari-21@users.noreply.github.com>
2022-03-15 23:25:51 +0530

    Update AzureInventory_V2.ps1

--- AzureInventory_V2.ps1
+++ AzureInventory_V2.ps1
@@ -1,5 +1,12 @@
 ﻿
-
+<#
+#Script: Azure Inventory Script
+#
+DESCRIPTION:
+This script will pull the infrastructure details of the Azure subscriptions. Details will be stored under the folder "c:\AzureInventory"
+If you have multiple subscriptions, a separate folder will be created for individual subscription.
+CSV files will be created for individual services (Virtual Machines, NSG rules, Storage Account, Virtual Networks, Azure Load Balancers) inside the subscription's directory
+#>
 function Invoke-GetAzureInventoryFunction{
     
     # Sign into Azure Portal
